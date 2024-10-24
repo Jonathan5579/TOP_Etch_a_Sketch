d3b06961e81b664cbbd4baad84927c8cb0ed0e69
Jonathan Briones <jonathan.briones@siix-global.com>
2024-10-24 15:29:55 -0600

    feat: add funcitons to generate square divs
       added the container and a button to generate
       the square divs that will act as pixel for
       drawing.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 console.log("hwllo world")
    
+const CONTAINER = document.querySelector("#container");
+const CreateSquaresButton = document.querySelector("#CreateSquaresButton")
 
-               
+function createSquareDiv(){
+   const squareDivs = document.createElement("div");
+   squareDivs.setAttribute("class", "square_div")
+   //squareDivs.textContent = ".";
+   //squareDivs.classList = "square_div";
+   CONTAINER.appendChild(squareDivs);
+}
+
+
+CreateSquaresButton.addEventListener('click', ()=>{
+   console.log("CreateSquaresButton");
+   createSquareDiv()
+});
