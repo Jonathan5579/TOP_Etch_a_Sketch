a22fb8181fdb4c2bf40578398fb3ea91c9d0cbaa
Jonathan Briones <jonathan.briones@siix-global.com>
2024-10-24 15:45:32 -0600

    feat: control box size and max width of container
       Implemented the functions to set the box
       size in the javascript. Compute the max
       width depending on the box individual width

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 console.log("hwllo world")
  //-CONSTANTS
-const SQUARE_WIDTH = 10;
+const SQUARE_WIDTH = 25;
 const SQUARE_HEIGHT = 25;
 const SQUARE_MARGIN = 2;
 const MAX_BOX_HORIZONTAL_ELEMENTS = 16;
 //-CONSTANTS
 
 const container = document.querySelector("#container");
-let maxContainerWidth = SQUARE_WIDTH * MAX_BOX_HORIZONTAL_ELEMENTS;
+let maxContainerWidth = (SQUARE_WIDTH + 2*SQUARE_MARGIN) * MAX_BOX_HORIZONTAL_ELEMENTS;
 container.setAttribute("style", `max-width: ${maxContainerWidth}px;`)
 const createSquaresButton = document.querySelector("#CreateSquaresButton")
 
 function createSquareDiv(){
    const squareDivs = document.createElement("div");
    squareDivs.setAttribute("class", "square_div")
+   squareDivs.setAttribute("style", `width: ${SQUARE_WIDTH}px; height: ${SQUARE_HEIGHT}px; margin: ${SQUARE_MARGIN}px;`)
+   
+   container.appendChild(squareDivs);
    squareDivs.setAttribute("style", `width: ${SQUARE_WIDTH}px; height: ${SQUARE_HEIGHT}px; margin: ${SQUARE_MARGIN}px;`)
    
    container.appendChild(squareDivs);
@@ -27,6 +30,15 @@ function generateNSquareDivs(n){
 }
 
 createSquaresButton.addEventListener('click', ()=>{
+function generateNSquareDivs(n){
+   
+   for (let i=0; i<n; i++){
+      createSquareDiv();
+   }
+}
+
+createSquaresButton.addEventListener('click', ()=>{
    console.log("CreateSquaresButton");
+   generateNSquareDivs(5);
    generateNSquareDivs(5);
 });
